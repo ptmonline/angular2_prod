09f99a3098a290c8fbcac4fa3e2e4cee191f87d1
Pau Torres <ptmonlinework@gmail.com>
2018-05-01 15:25:08 +0200

    added new auth test

---
+++ angular-unit-testing/src/app/auth/authservice.service.spec.ts
@@ -0,0 +1,23 @@
+import { AuthService } from "./authservice.service";
+
+describe('AuthService test', () => {
+    let service: AuthService;
+
+    beforeEach(() => {
+        service = new AuthService;
+    });
+
+    afterEach(() => {
+        service = null;
+        localStorage.removeItem('token');
+    })
+
+    it('should return something', () => {
+        localStorage.setItem('token', '1234');
+        expect(service.isAuthenticated()).toBeTruthy();
+    })
+
+    it('shuld return null or falsy', () => {
+        expect(service.isAuthenticated()).toBeFalsy();
+    })
+})
